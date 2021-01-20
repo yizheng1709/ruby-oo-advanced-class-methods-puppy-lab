c4018a5f7e53f967725713832232a07586efe2fa
Eva <yi.zheng1709@gmail.com>
2021-01-20 11:38:49 -0500

    commit tree b16f4ef3e40c7bad9e69e303fd46014e092ccba7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+class Dog
+    attr_reader :name 
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        self.save 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def save
+        @@all << self 
+    end  
+
+    def self.clear_all 
+        @@all = []
+    end 
+
+    def self.print_all 
+        @@all.each do |dog|
+           puts dog.name 
+        end 
+    end 
+end 
